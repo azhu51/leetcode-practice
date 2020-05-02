09ec363ec9579232f50e97b3a7e9406cc09bd41e
azhu51 <jawiezhu@qq.com>
2020-05-03 00:37:53 +0800

    update

--- contest/hard_5387.py
+++ contest/hard_5387.py
@@ -6,16 +6,10 @@ from typing import List
 class Solution:
   def numberWays(self, hats: List[List[int]]) -> int:
     if not hats: return 0
-
-    num = len(hats)
+    pass
 
 
 
 
 s = Solution()
 # print(s.numberWays(hats=[[3,4],[4,5],[5]]))
-
-res = set([])
-res.add([1,2,3,4])
-
-print(res)
