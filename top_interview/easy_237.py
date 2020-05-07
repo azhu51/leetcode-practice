04ba29e9793fc1ac4767a46b5c6ab4ab529dff1c
azhu51 <jawiezhu@qq.com>
2020-05-07 22:29:28 +0800

    update

--- top_interview/easy_237.py
+++ top_interview/easy_237.py
@@ -13,4 +13,4 @@ class Solution:
         :rtype: void Do not return anything, modify node in-place instead.
         """
         node.val = node.next.val
-        node.next = node.next.next 
+        node.next = node.next.next
